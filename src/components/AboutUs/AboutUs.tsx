15a5e6be69c9a8511b76cdeb726707f7556b0ef1
Ezequiel Sale <ezequielsale123@gmail.com>
2024-06-27 06:26:30 -0300

    se creo la vista de sobre nosotros

---
+++ src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+const AboutUs = () => {
+  return (
+    <>
+    <div className="card">
+        <div className="card2">
+            <div>
+                <Image src="" alt="logo" width={200} height={200} />
+            </div>
+            <div>
+                <p>Front end developer</p>
+            </div>
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default AboutUs
