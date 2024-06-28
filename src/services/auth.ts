c5c1381fc9d0f9399493fda85c95fc92a382f0b0
cos4h <olvadis2004@gmail.com>
2024-06-28 13:02:19 -0500

    Empty commit

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 
 export const login = async (email: string, password: string) => {
   try {
+    // proof of concept, do not use in production
     const response = await axios.post("http://localhost:3000/signin", {
       email,
       password,
