0399ea0216c2454a7a35f19cecd35bb4759067c7
Ezequiel Sale <ezequielsale123@gmail.com>
2024-06-28 11:05:39 -0300

    se creo las funciones para hacer las peticiones con  el back

--- src/helper/petitions.ts
+++ src/helper/petitions.ts
@@ -1,3 +1,5 @@
+import { ICredential } from "@/types/credentialInterface";
+import { IProfesor } from "@/types/profesorInterface";
 import { IFormValues } from "@/types/registerInterface";
 
 export const registerUser = async (user: IFormValues) => {
@@ -32,4 +34,78 @@ export async function getUsers() {
     }
   }
 
+  export async function createProfesor(profesor: IProfesor) {
+    try {
+      const response = await fetch('http://localhost:3001/profesor/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(profesor),
+      });
   
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+  
+      const createdProfesor = await response.json();
+      return createdProfesor;
+    } catch (error) {
+      console.error('Error creating profesor:', error);
+    }
+  }
+  
+  export async function getProfesors() {
+    try {
+      const response = await fetch('http://localhost:3001/profesor');
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      const users = await response.json();
+      return users;
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  }
+
+  async function updateUserStatus(id: string) {
+    try {
+      const response = await fetch(`http://localhost:3001/users/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({id}),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+  
+      const updatedUser = await response.json();
+      return updatedUser;
+    } catch (error) {
+      console.error('Error updating user status:', error);
+    }
+  }
+
+  async function loginUser(credentials: ICredential) {
+    try {
+      const response = await fetch('http://localhost:3001/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+  
+      const loginData = await response.json();
+      return loginData;
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  }
